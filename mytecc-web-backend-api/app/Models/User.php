4f190c824541a4e46e69fc7f61175b6ff40a4b26
Syahir Zakwan <syahirzakwanyusof@gmail.com>
2022-01-05 19:22:41 +0800

    Update users table migration and user model
    Added first_name, last_name, status, phone, address, postcode, city, state

--- mytecc-web-backend-api/app/Models/User.php
+++ mytecc-web-backend-api/app/Models/User.php
@@ -18,9 +18,17 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
+        'username',
+        'first_name',
+        'last_name',
         'email',
         'password',
+        'status',
+        'phone',
+        'address',
+        'postcode',
+        'city',
+        'state',
     ];
 
     /**
--- mytecc-web-backend-api/database/migrations/2014_10_12_000000_create_users_table.php
+++ mytecc-web-backend-api/database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,18 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('username')->unique();
+            $table->string('first_name');
+            $table->string('last_name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->enum('status', ['Enabled', 'Disabled'])->default('Enabled');
+            $table->string('phone')->nullable();
+            $table->string('address')->nullable();
+            $table->string('postcode')->nullable();
+            $table->string('city')->nullable();
+            $table->string('state')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
